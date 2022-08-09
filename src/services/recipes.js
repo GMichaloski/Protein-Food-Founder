aedbf0052c459cbdddf01a42abfaf1fb332d85b3
GMichaloski <gustavomichaloski@gmail.com>
2022-08-09 00:59:18 -0300

    :ambulance: fixed getRecipes

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,12 +1,15 @@
-import client from '../providers/client';
+import axios from 'axios';
 
-export const getRecipes = async data => {
-  const sufix = getSufix(data);
-  const results = await client.get(`?${sufix}`);
-  return { results };
+export const getRecipes = async ({ page = 0 }) => {
+  const sufix = getSufix({});
+  const results = await axios.get(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=${page}&${sufix}`
+  );
+  console.log(results);
+
+  return { data: results, nextPage: page + 1 };
 };
-
-const getSufix = object => {
+export const getSufix = object => {
   let sufix = '';
   if (object.minProtein) {
     sufix += `minProtein=${object.minProtein}&`;
