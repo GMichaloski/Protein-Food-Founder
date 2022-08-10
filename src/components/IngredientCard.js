df58a9f7d6cca7c17ba9b214d583b4391dba5440
GMichaloski <gustavomichaloski@gmail.com>
2022-08-10 18:47:59 -0300

    :sparkles: image and unit added to ingredientCard

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -3,25 +3,26 @@ import React from 'react';
 
 export default function IngredientCard(props) {
   const { name, image, metricMeasures } = props;
-  const { amount, unitLong } = metricMeasures;
-  console.log(metricMeasures);
+  const { amount, unitShort, unitLong } = metricMeasures;
+  const imageURL = `https://spoonacular.com/cdn/ingredients_500x500/${image}`;
   return (
     <Box
       bgColor="white"
       borderRadius="full"
       width="90vw"
-      height="115px"
+      height="100px"
       boxShadow="2xl"
       marginTop="10px"
     >
       <Grid templateRows="(repeat(1, 1fr))" templateColumns="repeat(2, 1fr)">
-        <Image src={image} boxSize="115px" borderRadius="full" />
-        <Box marginTop="20px" paddingRight="30px">
+        <Image src={imageURL} boxSize="100px" borderRadius="full" />
+        <Box marginTop="10px" paddingRight="30px">
           <Text color="black" fontSize="xl" alignSelf="flex-start">
             {name}
           </Text>
           <Text color="black" fontSize="xl" alignSelf="flex-start">
-            {Math.ceil(amount)} {unitLong}
+            {Math.ceil(amount)}{' '}
+            {unitShort ? unitShort : amount > 1 ? 'units' : 'unit'}
           </Text>
         </Box>
       </Grid>
