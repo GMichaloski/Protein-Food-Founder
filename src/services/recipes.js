e0cf959c104ebb98ad11e75af04f938791036901
GMichaloski <gustavomichaloski@gmail.com>
2022-08-10 18:15:56 -0300

    :sparkles: getSpecificRecipe added and getRecipes improved

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
-export const getRecipes = async ({ page = 0 }) => {
-  const sufix = getSufix({});
+export const getRecipes = async ({ page = 0 }, searchParams) => {
+  const sufix = getSufix(searchParams);
   const results = await axios.get(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=${page}&${sufix}`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+      process.env.REACT_APP_API_KEY
+    }&offset=${page * 10}&number=10&${sufix}`
   );
-  console.log(results);
-
-  return { data: results, nextPage: page + 1 };
+  return { data: results.data.results, nextPage: page + 1 };
 };
+
+export const getSpecificRecipe = async id => {
+  const result = await axios.get(
+    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+  );
+  return result;
+};
+
 export const getSufix = object => {
   let sufix = '';
   if (object.minProtein) {
