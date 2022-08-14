a814479cd519ea1d47f181cbdfcd830d1455ce74
GMichaloski <gustavomichaloski@gmail.com>
2022-08-14 14:42:55 -0300

    :sparkles: return button component added

---
+++ src/components/ReturnButton.js
@@ -0,0 +1,22 @@
+import { Box, IconButton } from '@chakra-ui/react';
+import React from 'react';
+import { BsArrowReturnLeft } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+export default function ReturnButton() {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate('/', { replace: true });
+  };
+  return (
+    <Box position="fixed" top="10px" left="10px">
+      <IconButton
+        aria-label="return"
+        icon={<BsArrowReturnLeft />}
+        variant="outline"
+        colorScheme="black"
+        bg="black"
+        onClick={onClick}
+      />
+    </Box>
+  );
+}
